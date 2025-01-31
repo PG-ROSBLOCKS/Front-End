e8f5b357884b3673cde01aba78e93c832e94c950
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2025-01-31 18:03:41 -0500

    Correcciones de CSS

--- ROSBLOCKS/src/app/shared/components/header/header.component.ts
+++ ROSBLOCKS/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  mostrarExportar: boolean = true;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.mostrarExportar = this.router.url !== '/help';
+    });
+  }
 }
