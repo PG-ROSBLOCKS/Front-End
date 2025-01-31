d77c5091539bf7e041511f65647ffc7a2280afb9
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2025-01-31 10:23:28 -0500

    Blockly para cada pestaña (Falta corregir como se inicia la primera vez)

--- ROSBLOCKS/src/app/workspace/workspace.component.ts
+++ ROSBLOCKS/src/app/workspace/workspace.component.ts
@@ -9,123 +9,94 @@ import * as Blockly from 'blockly';
 export class WorkspaceComponent implements AfterViewInit {
   workspace: Blockly.WorkspaceSvg | undefined;
   
-    // Toolbox inicial (Los bloques aqui son solo de prueba)
-    toolbox = {
-      kind: 'categoryToolbox',
-      contents: [
-        {
-          kind: 'category',
-          name: 'Nodes',
-          contents: [
-            { kind: 'block', type: 'controls_if' },
-            { kind: 'block', type: 'logic_compare' },
-            { kind: 'block', type: 'logic_operation' },
-            { kind: 'block', type: 'logic_negate' },
-            { kind: 'block', type: 'logic_boolean' },
-          ],
-        },
-        {
-          kind: 'category',
-          name: 'Services',
-          contents: [
-            { kind: 'block', type: 'controls_repeat_ext' },
-            { kind: 'block', type: 'controls_whileUntil' },
-            { kind: 'block', type: 'controls_for' },
-            { kind: 'block', type: 'controls_flow_statements' },
-          ],
-        },
-        {
-          kind: 'category',
-          name: 'Topics',
-          contents: [
-            { kind: 'block', type: 'math_number' },
-            { kind: 'block', type: 'math_arithmetic' },
-            { kind: 'block', type: 'math_single' },
-            { kind: 'block', type: 'math_trig' },
-            { kind: 'block', type: 'math_random_int' },
-          ],
-        },
-        {
-          kind: 'category',
-          name: 'etc',
-          contents: [
-            { kind: 'block', type: 'math_number' },
-            { kind: 'block', type: 'math_arithmetic' },
-            { kind: 'block', type: 'math_single' },
-            { kind: 'block', type: 'math_trig' },
-            { kind: 'block', type: 'math_random_int' },
-          ],
-        },
-      ],
-    };
-  
-    ngAfterViewInit(): void {
-      this.initializeBlockly();
-    }
-  
-    initializeBlockly(): void {
-      this.workspace = Blockly.inject('blocklyDiv', {
-        toolbox: this.toolbox,
-        trashcan: true,
-        /*
-              grid: {
-          spacing: 20,
-          length: 3,
-          colour: '#ccc',
-          snap: true
-        },
-        */
-        zoom: {
-          controls: true,
-          wheel: true,
-          startScale: 0.7,
-          maxScale: 2,
-          minScale: 0.6,
-          scaleSpeed: 1.1
-        },
-        move: {
-          scrollbars: true,
-          drag: true,
-          wheel: false
-        },
-        sounds: true,
-        media: 'https://unpkg.com/blockly/media/',
-        rtl: false,
-        horizontalLayout: false,
-        renderer: 'zelos',
-        theme: Blockly.Themes.Classic
-      });
-    }
-  
-    onSearch(event: any): void {
-      const query = event.target.value.toLowerCase();
-  
-      // Filtrar categorías y bloques por el texto ingresado
-      const filteredToolbox = {
-        kind: 'categoryToolbox',
-        contents: this.toolbox.contents
-          .map((category: any) => {
-            // Filtrar bloques dentro de la categoría
-            const filteredContents = category.contents.filter((block: any) =>
-              block.type.toLowerCase().includes(query)
-            );
-  
-            // Incluir la categoría solo si tiene bloques que coincidan
-            if (filteredContents.length > 0) {
-              return { ...category, contents: filteredContents };
-            }
-            return null;
-          })
-          .filter((category: any) => category !== null), // Eliminar categorías vacías
-      };
-  
-      // Actualizar el toolbox con los resultados filtrados
-      this.workspace?.updateToolbox(filteredToolbox);
-    }
+  // Toolbox inicial (Los bloques aqui son solo de prueba)
+  toolbox = {
+    kind: 'categoryToolbox',
+    contents: [
+      {
+        kind: 'category',
+        name: 'Nodes',
+        contents: [
+          { kind: 'block', type: 'controls_if' },
+          { kind: 'block', type: 'logic_compare' },
+          { kind: 'block', type: 'logic_operation' },
+          { kind: 'block', type: 'logic_negate' },
+          { kind: 'block', type: 'logic_boolean' },
+        ],
+      },
+      {
+        kind: 'category',
+        name: 'Services',
+        contents: [
+          { kind: 'block', type: 'controls_repeat_ext' },
+          { kind: 'block', type: 'controls_whileUntil' },
+          { kind: 'block', type: 'controls_for' },
+          { kind: 'block', type: 'controls_flow_statements' },
+        ],
+      },
+      {
+        kind: 'category',
+        name: 'Topics',
+        contents: [
+          { kind: 'block', type: 'math_number' },
+          { kind: 'block', type: 'math_arithmetic' },
+          { kind: 'block', type: 'math_single' },
+          { kind: 'block', type: 'math_trig' },
+          { kind: 'block', type: 'math_random_int' },
+        ],
+      },
+      {
+        kind: 'category',
+        name: 'etc',
+        contents: [
+          { kind: 'block', type: 'math_number' },
+          { kind: 'block', type: 'math_arithmetic' },
+          { kind: 'block', type: 'math_single' },
+          { kind: 'block', type: 'math_trig' },
+          { kind: 'block', type: 'math_random_int' },
+        ],
+      },
+    ],
+  };
 
-    tabs: { name: string, id: number, isPlaying: boolean }[] = [];
+  tabs: { name: string, id: number, isPlaying: boolean }[] = [];
   selectedTabId: number | null = null;
   selectedTabName: string | null = null;
+
+  ngAfterViewInit(): void {
+    // Inicialización de la primera pestaña
+    if (this.tabs.length > 0) {
+      this.selectTab(this.tabs[0].id);
+    }
+  }
+
+  initializeBlockly(tabId: number): void {
+    // El nombre del div será único por pestaña
+    const blocklyDivId = `blocklyDiv-${tabId}`;
+    this.workspace = Blockly.inject(blocklyDivId, {
+      toolbox: this.toolbox,
+      trashcan: true,
+      zoom: {
+        controls: true,
+        wheel: true,
+        startScale: 0.7,
+        maxScale: 2,
+        minScale: 0.6,
+        scaleSpeed: 1.1
+      },
+      move: {
+        scrollbars: true,
+        drag: true,
+        wheel: false
+      },
+      sounds: true,
+      media: 'https://unpkg.com/blockly/media/',
+      rtl: false,
+      horizontalLayout: false,
+      renderer: 'zelos',
+      theme: Blockly.Themes.Classic
+    });
+  }
 
   addTab() {
     if (this.tabs.length >= 5) {
@@ -134,14 +105,17 @@ export class WorkspaceComponent implements AfterViewInit {
     }
     const newTabId = this.tabs.length + 1;
     this.tabs.push({ name: `Nodo ${newTabId}`, id: newTabId, isPlaying: false });
+
+    // Selecciona la nueva pestaña y abre su espacio de trabajo
     this.selectTab(newTabId);
   }
-  
 
   selectTab(tabId: number) {
     this.selectedTabId = tabId;
-    const selectedTab = this.tabs.find(tab => tab.id === tabId);
-    this.selectedTabName = selectedTab ? selectedTab.name : null;
+    this.selectedTabName = this.tabs.find(tab => tab.id === tabId)?.name || null;
+
+    // Inicializar Blockly solo cuando se selecciona la pestaña
+    this.initializeBlockly(tabId);
   }
 
   changeTabName(tabId: number, newName: string) {
@@ -163,5 +137,29 @@ export class WorkspaceComponent implements AfterViewInit {
     if (this.selectedTabId === tabId) {
       this.selectedTabId = this.tabs.length > 0 ? this.tabs[0].id : null;
     }
+  }
+
+
+  onSearch(event: any): void {
+    const query = event.target.value.toLowerCase();
+
+    // Filtrar categorías y bloques por el texto ingresado
+    const filteredToolbox = {
+      kind: 'categoryToolbox',
+      contents: this.toolbox.contents
+        .map((category: any) => {
+          // Filtrar bloques dentro de la categoría
+          const filteredContents = category.contents.filter((block: any) =>
+            block.type.toLowerCase().includes(query)
+          );
+
+          // Incluir la categoría solo si tiene bloques que coincidan
+          if (filteredContents.length > 0) {
+            return { ...category, contents: filteredContents };
+          }
+          return null;
+        })
+        .filter((category: any) => category !== null), // Eliminar categorías vacías
+    };
   }
 }
