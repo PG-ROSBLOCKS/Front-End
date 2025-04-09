d0aba86f144d822d04b72970839f3f4998603224
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2025-04-08 19:12:58 -0500

    feat: Some tutorials

--- ROSBLOCKS/src/app/rosblocks-help/rosblocks-help.component.ts
+++ ROSBLOCKS/src/app/rosblocks-help/rosblocks-help.component.ts
@@ -18,36 +18,6 @@ export class RosblocksHelpComponent {
   }
 
   selectVideo(videoName: string): void {
-    switch(videoName) {
-      case 'video1':
-        this.selectedVideo = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
-        break;
-      case 'video2':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      case 'video3':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      case 'videoA':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      case 'videoB':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      case 'videoC':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      case 'videoX':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      case 'videoY':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      case 'videoZ':
-        this.selectedVideo = 'https://www.youtube.com/embed/eudOwe3Rz-0?si=uLdWZQYNo_lg8UF_';
-        break;
-      default:
-        this.selectedVideo = null;
-    }
+    this.selectedVideo = videoName;
   }
 }
