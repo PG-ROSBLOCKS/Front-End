d1914c0f12d5c45938dcdcb82857b5d5cc46ddd7
Kevin Estiven Velasco Pinto <kevinevelasco@javeriana.edu.co>
2025-04-22 18:46:23 -0500

    IP

--- ROSBLOCKS/src/app/services/code.service.ts
+++ ROSBLOCKS/src/app/services/code.service.ts
@@ -17,7 +17,7 @@ export class CodeService {
   noBlocks$ = this.noBlocksSubject.asObservable();
   private API_URL = 'http://localhost:8000';
   private API_URL_NO_PORT = 'http://localhost:';
-  private API_CONTAINER_IP = '54.87.7.111';
+  private API_CONTAINER_IP = '54.147.32.204';
 
   constructor(private http: HttpClient) {
     this.wsSubject = undefined;
